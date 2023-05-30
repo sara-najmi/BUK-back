fb3949d462fd64231be66b2747df61c1f2fec552
Moh3n <amini0348@gmail.com>
2023-05-30 11:12:15 +0330

    config

--- src/main/java/com/sbuk/shopping/product/controller/ProductModel.java
+++ src/main/java/com/sbuk/shopping/product/controller/ProductModel.java
@@ -1,6 +1,7 @@
 package com.sbuk.shopping.product.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
@@ -8,6 +9,7 @@ import java.math.BigDecimal;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class ProductModel {
     private Integer id;
     private String title;
